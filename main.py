b3f09ef39cb5901198ca0b13c42a72a5c17980e5
nitesh <nitesh26028@gmail.com>
2024-04-01 23:10:21 +0530

    t

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from flask import Flask
-from flask import render_template
+from flask import render_template,request
 
 
 app=Flask(__name__)
@@ -17,6 +17,14 @@ def contactus():
 @app.route("/services")
 def services():
     return render_template("services.html")
+@app.route("/savethisdata",methods=["post"])
+def savethisdata():
+    if request.method =="POST":
+        mytitle=request.form.get("title")
+        message=request.form.get("msg")
+
+        print(mytitle,message)
+    return "your data is suceessfully............."
 
 if __name__=="__main__":
     app.run(debug=True)
