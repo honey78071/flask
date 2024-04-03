0a66c4d0123421a4b50a5ee1f795d94e83b3939e
nitesh <nitesh26028@gmail.com>
2024-04-03 15:12:48 +0530

    i

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 from flask import Flask
-from flask import render_template,request
-
+from flask import render_template,request,redirect
+# from flask _sqlalchemy import SQLAlchemy
+import mysql.connector 
+conn = mysql.connector.connect(host="localhost",username="root",password="12345",database="flaskdata")
+cuser=conn.cursor()
 
 app=Flask(__name__)
 
@@ -16,15 +19,24 @@ def contactus():
     return render_template("contactus.html")
 @app.route("/services")
 def services():
-    return render_template("services.html")
+    cuser.execute("select * from flasksave")
+    data=cuser.fetchall()
+    return render_template("services.html",mydata=data)
 @app.route("/savethisdata",methods=["post"])
 def savethisdata():
     if request.method =="POST":
         mytitle=request.form.get("title")
         message=request.form.get("msg")
-
-        print(mytitle,message)
+        cuser.execute(f"insert into flasksave values('{mytitle}','{message}')")
+        conn.commit()
+        return redirect("/contactus")
     return "your data is suceessfully............."
+@app.route("/deletethisdata/<x>",methods=["POST"])
+def deletethisdata(x):
+    cuser.execute(f"delete from flasksave where title='{x}'")
+    return"data deleteeeeeeeeeeeeeeeeee"
 
 if __name__=="__main__":
     app.run(debug=True)
+
+# orm:sqlalchemy
